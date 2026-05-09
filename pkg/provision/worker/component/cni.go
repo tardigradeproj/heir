08d8b4c3eb87001bf66e07d15a39919f372b338d
Edwin <edwinmarrima@gmail.com>
2026-05-09 21:37:03 +0200

    feat: add CNI plugin runner component
    Implement the Cni Runner that extracts CNI plugin binaries (bridge,
    dhcp, firewall, host-local, host-device, ipvlan, portmap, ptp, static,
    tap) into CNIBinFolderPath during Setup. Run and Teardown are no-ops
    since CNI plugins are invoked by the container runtime, not managed as
    long-running processes.

--- pkg/provision/worker/component/cni.go
+++ pkg/provision/worker/component/cni.go
@@ -1,1 +1,48 @@
 package component
+
+import (
+	"context"
+	"fmt"
+	"path"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/tardigrade-runtime/samaritano/pkg/provision/worker/typ"
+)
+
+type Cni struct {
+	wrkCtx *typ.WorkerContext
+}
+
+func NewCni(wrkCtx *typ.WorkerContext) *Cni {
+	return &Cni{wrkCtx: wrkCtx}
+}
+
+func (c *Cni) Setup() error {
+	binaries := []struct{ src, dst string }{
+		{"worker/cni/bridge", path.Join(c.wrkCtx.CNIBinFolderPath, "bridge")},
+		{"worker/cni/dhcp", path.Join(c.wrkCtx.CNIBinFolderPath, "dhcp")},
+		{"worker/cni/firewall", path.Join(c.wrkCtx.CNIBinFolderPath, "firewall")},
+		{"worker/cni/static", path.Join(c.wrkCtx.CNIBinFolderPath, "static")},
+		{"worker/cni/host-device", path.Join(c.wrkCtx.CNIBinFolderPath, "host-device")},
+		{"worker/cni/host-local", path.Join(c.wrkCtx.CNIBinFolderPath, "host-local")},
+		{"worker/cni/tap", path.Join(c.wrkCtx.CNIBinFolderPath, "tap")},
+		{"worker/cni/ipvlan", path.Join(c.wrkCtx.CNIBinFolderPath, "ipvlan")},
+		{"worker/cni/portmap", path.Join(c.wrkCtx.CNIBinFolderPath, "portmap")},
+		{"worker/cni/ptp", path.Join(c.wrkCtx.CNIBinFolderPath, "ptp")},
+	}
+	for _, b := range binaries {
+		log.WithField("dst", b.dst).Info("extracting binary")
+		if err := extractStreamed(b.src, b.dst); err != nil {
+			return fmt.Errorf("failed to extract %s: %w", b.src, err)
+		}
+	}
+	return nil
+}
+
+func (c *Cni) Run(_ context.Context) error {
+	return nil
+}
+
+func (c *Cni) Teardown(_ context.Context) error {
+	return nil
+}
