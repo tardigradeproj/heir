f6d31e976e495670d30298d028f18f0b61d8d83f
Edwin <edwinmarrima@gmail.com>
2026-06-10 20:42:02 +0200

    feat: compress artifacts

--- artifacts/embed_arm64.go
+++ artifacts/embed_arm64.go
@@ -4,5 +4,5 @@ package artifact
 
 import "embed"
 
-//go:embed worker/arm64/kubelet worker/arm64/containerd worker/arm64/runc worker/arm64/containerd-shim-runc-v2 worker/arm64/ctr worker/arm64/crictl worker/arm64/cni
+//go:embed worker/arm64/kubelet.zst worker/arm64/containerd.zst worker/arm64/runc.zst worker/arm64/containerd-shim-runc-v2.zst worker/arm64/ctr.zst worker/arm64/crictl.zst worker/arm64/cni.tar.zst
 var FS embed.FS
